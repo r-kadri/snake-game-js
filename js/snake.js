8eed786dc7934f6b9a681aafb4e84bf05a896b99
Ryan Kadri <ryankadri.dev@gmail.com>
2022-11-02 17:03:08 +0100

    snake movements with keyboard

--- js/snake.js
+++ js/snake.js
@@ -1,17 +1,32 @@
-export const SNAKE_SPEED = 2; // In a second
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5; // In a second
 const snakeBody = [
     {x: 0, y: 0}
 ]
 
 export function update() {
+    const inputDirection = getInputDirection()
+
     // MOVE SNAKE BODY EXCEPT THE HEAD
     for (let index = snakeBody.length -2; index >= 0; index--) {
-        snakeBody[index+1] = snakeBody[index]
+        snakeBody[index+1] = { ...snakeBody[index] }
     }
 
     // SNAKE'S HEAD
-    /*snakeBody[0].x = -1
-    snakeBody[0].y = -1*/
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
+
+    // NO BORDERS
+    if(snakeBody[0].x == 22)
+        snakeBody[0].x = 0
+    else if(snakeBody[0].x == -1)
+        snakeBody[0].x = 21
+
+    if(snakeBody[0].y == 22)
+        snakeBody[0].y = 0
+    else if(snakeBody[0].y == -1)
+        snakeBody[0].y = 21
 }
 
 export function draw(game) {
